70430d8226e32d7b5abf25c88eb32482a1fe6318
William <willgithub@outlook.com>
2021-03-06 20:30:02 -0500

    Fixed PKCE Code Generation

--- util/PKCE.js
+++ util/PKCE.js
@@ -1,4 +1,20 @@
 
+function sha256(plain) {
+    // returns promise ArrayBuffer
+    const encoder = new TextEncoder();
+    const data = encoder.encode(plain);
+    return window.crypto.subtle.digest('SHA-256', data);
+}
+
+function base64urlencode(a) {
+    // Convert the ArrayBuffer to string using Uint8 array.
+    // btoa takes chars from 0-255 and base64 encodes.
+    // Then convert the base64 encoded to base64url encoded.
+    // (replace + with -, replace / with _, trim trailing =)
+    return btoa(String.fromCharCode.apply(null, new Uint8Array(a)))
+        .replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+}
+
 // Generates code challenge for Spotify PKCE authorization
 async function PKCE(clientId, redirectURI) {
     // Generate 128 character string
@@ -13,11 +29,8 @@ async function PKCE(clientId, redirectURI) {
     setCookie('codeVerifier', codeVerifier, 1);
 
     // Encode string
-    const encoder = new TextEncoder();
-    const data = encoder.encode(codeVerifier);
-    const hashed = window.crypto.subtle.digest('SHA-256', data);
-    const codeChallenge = btoa(String.fromCharCode.apply(null, new Uint8Array(hashed))
-        .replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, ''));
+    var hashed = await sha256(codeVerifier);
+    var codeChallenge = base64urlencode(hashed);
 
     // Redirect to Spotify authorization API
     window.location.replace(
